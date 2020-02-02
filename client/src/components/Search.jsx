9fd9e0c90afee49120dfd483fe3603fc27a3b98f
sebekxp <p15sebek@gmail.com>
2020-02-02 14:40:35 +0100

    Changed search form to reactstrap component.

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Search as searchIcon } from 'styled-icons/icomoon/Search';
+import { FormGroup, Input } from 'reactstrap';
 import { connect } from 'react-redux';
-import Colors from './Colors';
 
 const Search = (props) => {
-    const {words} = props;
+    const { words } = props;
 
     const handleInputChange = (e) => {
         const inputValue = e.target.value.toLowerCase().trim();
@@ -22,44 +20,26 @@ const Search = (props) => {
         }
     };
 
-    const SearchContainer = styled.div`
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        border-radius: 10px;
-        box-shadow: ${Colors.BOX_SHADOW};
-        height: 50px;
-        width: 360px;
-        margin: auto;
-        position: fixed;
-        top: 100px;
-        left: 0;
-        right: 0;
-    `;
+    const formGroupStyle = {
+        width: '360px',
+        margin: '100px auto'
+    };
 
-    const SearchInput = styled.input`
-        padding: 0 5px;
-        border-radius: 10px;
-        font-size: 20px;
-        height: 100%    ;
-        width: 100%;
-        outline: none;
-    `;
-
-    const SearchIcon = styled(searchIcon)`
-        position: absolute;
-        color: ${Colors.DEFAULT_GREY};
-        right: 10px;
-        width: 25px;
-        height: 25px;
-    `;
+    const inputStyle = {
+        fontSize: '18px'
+    };
 
     return (
-        <SearchContainer>
-            <SearchInput type="text" name="" id="search" placeholder="Search word ..."
-                         onChange={(e) => handleInputChange(e)}/>
-            <SearchIcon/>
-        </SearchContainer>
+        <FormGroup style={formGroupStyle}>
+            <Input
+                style={inputStyle}
+                type="search"
+                name="search"
+                id="exampleSearch"
+                placeholder="Search word..."
+                onChange={(e) => handleInputChange(e)}
+            />
+        </FormGroup>
     );
 };
 
