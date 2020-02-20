c6489f62a5869e838a7f676c53f381166dbc4121
sebekxp <p15sebek@gmail.com>
2020-02-20 22:46:08 +0100

    Added userpreferences, fix keepeing user loged in when refresh page, clean code

--- client/src/components/auth/actions/setAuthToken.js
+++
@@ -1,9 +0,0 @@
-const setAuthToken = (token, options) => {
-    if (token) {
-        options.Authorization = token;
-    } else {
-        delete options.Authorization;
-    }
-};
-
-export default setAuthToken;
